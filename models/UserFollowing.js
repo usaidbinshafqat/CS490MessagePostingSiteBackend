517e83f1aed02a9eddba40a9db0cfcefa834847f
mallorydolorfino <77677387+mallorydolorfino@users.noreply.github.com>
2022-11-15 18:46:51 -0500

    changed userfollowing table

--- models/UserFollowing.js
+++ models/UserFollowing.js
@@ -5,7 +5,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false
     },
     Following: {
-      type: DataTypes.BOOLEAN,
+      type: DataTypes.INTEGER,
       allowNull: false
     }
   })
